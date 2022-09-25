14fe276851d63a0ea57cc8cf9cac27ded01e6aef
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-25 15:42:17 +0200

    javascript changes with button click

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const changeTextButtonElem = document.querySelector(".changeTextButton");
 
 messageElem.innerText = "this is from Javascript";
 changeTextButtonElem.addEventListener("click", () => {
-    console.log("button was clicked");
+    messageElem.innerText = "TEXT WAS CHANGED";
 });
