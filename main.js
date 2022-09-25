796f22ebd9b1449c6ef2ab9f02675bcfdf75f998
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-25 18:18:11 +0200

    toogle button

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const questionElem = document.querySelector(".question");
 const answerElem = document.querySelector(".answer");
 const showAnswerButtonElem = document.querySelector(".showAnswerButton");
 
-messageElem.innerText = "this is from Javascript";
-changeTextButtonElem.addEventListener("click", () => {
-    messageElem.innerText = "TEXT WAS CHANGED";
+showAnswerButtonElem.addEventListener("click", () => {
+    answerElem.classList.toggle("hidden");
 });
